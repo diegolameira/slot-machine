341183b04542421c9b6ad1cb08409b1d3d5e13f1
Diego Lameira <diego.lameira@dextra-sw.com>
2019-01-24 02:13:04 -0200

    global styles

--- src/SlotMachine/index.js
+++ src/SlotMachine/index.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: sans-serif;
+    text-align: center;
+  }
+`;
 
 export default class SlotMachine extends Component {
   render() {
-    return <h1>Slot Machine</h1>;
+    return (
+      <>
+        <GlobalStyles />
+        <h1>Slot Machine</h1>
+      </>
+    );
   }
 }
